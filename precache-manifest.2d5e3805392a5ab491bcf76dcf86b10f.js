7a2c8c83cacc0a480b1cd563c3ea23cfe2b3a4bb
DanyalJanjua <daniafzal29@gmail.com>
2019-08-18 15:55:13 +0500

    Updates

---
+++ precache-manifest.2d5e3805392a5ab491bcf76dcf86b10f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3d1239eaf0ca25b73c28d650815189",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "5c3ae1a51fb84aecd154",
+    "url": "/test/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "1559f9117e902a26adf8",
+    "url": "/test/static/css/main.60c4f410.chunk.css"
+  },
+  {
+    "revision": "5c3ae1a51fb84aecd154",
+    "url": "/test/static/js/2.e2f21638.chunk.js"
+  },
+  {
+    "revision": "1559f9117e902a26adf8",
+    "url": "/test/static/js/main.9e1f8b15.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  }
+]);
